82c42ef5e8a55e0a4f80aede4d395a8d6c9cc824
Leref <Juansantana75@icloud.com>
2020-11-28 21:16:03 -0500

    Improved server.js

--- server.js
+++ server.js
@@ -4,10 +4,37 @@ const bot = new dbd.Bot({
 token: "TOKEN", 
 prefix: "!" 
 })
- 
+
 bot.onMessage()
- 
+//commands
+
 bot.command({
 name: "ping", 
 code: `Pong! \`$ping\`` 
 })
+
+bot.command({
+name: "trigger", 
+code: `code` 
+})
+
+//status
+
+bot.status({
+  text: "Bot is running!",
+  type: "PLAYING",
+  time: 12
+})
+
+bot.status({
+  text: "Add as many you need!",
+  type: "PLAYING",
+  time: 12
+})
+
+//variables
+
+bot.variables({
+    Name: "Value",
+    Name2: "Value2"
+  })
